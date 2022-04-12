cf18f98449ec5f4d3574bf13a3e6ccbb6c90677d
Magda <m.stebel@mailplus.pl>
2022-04-12 20:35:59 +0200

    Rozwiązanie zadania 19_2 poprawki

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 function validatePassword() {
     const password = document.getElementById("user-password");
-    // const passwordConfirmation = document.getElementById("password-confirmation");
     password.addEventListener("keyup", checkConditions);
 }
 validatePassword();
 
+function confirmPassword () {
+const passwordConfirmation = document.getElementById("password-confirmation");
+    passwordConfirmation.addEventListener("keyup", () => {
+    let userInput = document.getElementById("user-password").value;
+    let passwordConfirmationStyle = document.getElementById("password-confirmation-error-message").style;
+    let passwordConfirmationValue = passwordConfirmation.value;
+    passwordConfirmationStyle.display = passwordConfirmationValue && passwordConfirmationValue === userInput ? "none" : "block";
+    })
+}
+confirmPassword();
+
 function checkConditions() {
     let userInput = document.getElementById("user-password").value;
 
-    /[a-z]/.test(userInput) ? document.getElementById("small-letter").style.display = "none" :
-        document.getElementById("small-letter").style.display = "block";
+    let smallLetterStyle = document.getElementById("small-letter").style;
+    smallLetterStyle.display = /[a-z]/.test(userInput) ? "none" : "block";
 
+    let capitalLetterStyle = document.getElementById("capital-letter").style;
+    capitalLetterStyle.display = /[A-Z]/.test(userInput) ?"none": "block";
 
-    /[A-Z]/.test(userInput) ? document.getElementById("capital-letter").style.display = "none" :
-        document.getElementById("capital-letter").style.display = "block";
+    let eightSymbolsStyle = document.getElementById("eight-symbols").style;
+    eightSymbolsStyle.display = userInput.length >= 8 ? "none" : "block";
 
-    userInput.length >= 8 ? document.getElementById("eight-symbols").style.display = "none"
-        : document.getElementById("eight-symbols").style.display = "block";
-
-    /[!@#$%^&*(),.?":{}|<>]/.test(userInput) ? document.getElementById("special-character").style.display = "none"
-        : document.getElementById("special-character").style.display = "block";
-
-    const passwordConfirmation = document.getElementById("password-confirmation");
-    passwordConfirmation.addEventListener("keyup", () => {
-        passwordConfirmation.value === userInput
-            ? document.getElementById("password-confirmation-error-message").style.display = "none" :
-            document.getElementById("password-confirmation-error-message").style.display = "block";
-    })
+    let specialCharacterStyle = document.getElementById("special-character").style;
+    specialCharacterStyle.display = /[!@#$%^&*(),.?":{}|<>]/.test(userInput) ? "none" : "block";
 }
